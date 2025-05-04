fc5b5439cb33bbdd8eba1b0018381ba9ac41f036
LinZhicong <779170046@qq.com>
2025-05-04 18:45:19 +0800

    v5.5

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-    baseURL: 'https://s20.iems5718.ie.cuhk.edu.hk',  // 更新为你的域名
+    baseURL: 'https://s20.iems5718.ie.cuhk.edu.hk:8000',  // 更新为你的域名
     timeout: 5000
 })
 
